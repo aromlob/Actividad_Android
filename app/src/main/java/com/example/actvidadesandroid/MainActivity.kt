7f10cbd925ea775bc1d1fb926890de0b879517a7
aromlob <aromerolob@gmail.com>
2024-10-17 13:54:01 +0200

    Primera Parte Actividad 5

--- app/src/main/java/com/example/actvidadesandroid/MainActivity.kt
+++ app/src/main/java/com/example/actvidadesandroid/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.actvidadesandroid
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+//Actvidad 5: Ciclo de Vida
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +21,29 @@ class MainActivity : AppCompatActivity() {
         val textView : TextView = findViewById(R.id.iv_text)
         textView.setText("Actividad 2 Cambio color")
 
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "Se llama después de onCreate. Santi, muestro la pantalla. El usuario aún no puede interaccionar")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "El usuario ya puede interaccionar con la pantalla")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "Pierdo el foco de la pantalla. Boton home")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "Vuelvo a estar visible para santi.")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "Dejo de estar visible para el usuario. Otra App, S.O.")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "La actividad muere.")
     }
 }
