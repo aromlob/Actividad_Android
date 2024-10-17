a7cdb681dab01331cef3fae098d11cf906f07867
aromlob <aromerolob@gmail.com>
2024-10-17 17:05:10 +0200

    Primera Parte Actividad 7

--- app/src/main/java/com/example/actvidadesandroid/MainActivity.kt
+++ app/src/main/java/com/example/actvidadesandroid/MainActivity.kt
@@ -12,10 +12,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-//Actvidad 5: Ciclo de Vida
+
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity(), OnClickListener {
+
     private lateinit var boton : Button
     private lateinit var texto : TextView
     private lateinit var boton3 : Button
@@ -29,31 +30,6 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         initEvent()
 
     }
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "Se llama después de onCreate. Santi, muestro la pantalla. El usuario aún no puede interaccionar")
-    }
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "El usuario ya puede interaccionar con la pantalla")
-    }
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "Pierdo el foco de la pantalla. Boton home")
-    }
-    override fun onRestart() {
-        super.onRestart()
-        Log.d(TAG, "Vuelvo a estar visible para santi.")
-    }
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "Dejo de estar visible para el usuario. Otra App, S.O.")
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "La actividad muere.")
-    }
-
     private fun initEvent(){
         boton.setOnClickListener{
             Toast.makeText(this, "Hola!!!!",Toast.LENGTH_LONG).show()
@@ -61,6 +37,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         boton3.setOnClickListener(this)
 
     }
+
     fun saludar(view: View) {
         if (view. id == R.id. iv_boton) {//podría quitarlo, porque sólo lo referencia un sólo botón desde la interfaz
             Toast.makeText( this, "No quiero saludar!!!",Toast.LENGTH_LONG).show()
