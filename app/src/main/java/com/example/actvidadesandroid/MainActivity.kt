940f6cc5e53cebee29d0862b29698aacc1bac0c4
aromlob <aromerolob@gmail.com>
2024-10-17 14:38:52 +0200

    Segunda Parte Actividad 5

--- app/src/main/java/com/example/actvidadesandroid/MainActivity.kt
+++ app/src/main/java/com/example/actvidadesandroid/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.actvidadesandroid
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,15 +15,18 @@ import androidx.core.view.WindowInsetsCompat
 //Actvidad 5: Ciclo de Vida
 private const val TAG = "MainActivity"
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
+    private lateinit var boton : Button
+    private lateinit var texto : TextView
+    private lateinit var boton3 : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val imageView : ImageView = findViewById(R.id.iv_imagen)
-        imageView.setImageResource(R.drawable.imagen)
-        val textView : TextView = findViewById(R.id.iv_text)
-        textView.setText("Actividad 2 Cambio color")
+        boton = findViewById(R.id.iv_boton)
+        texto = findViewById(R.id.iv_texto)
+        boton3 = findViewById(R.id.button3)
+        initEvent()
 
     }
     override fun onStart() {
@@ -46,4 +53,33 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         Log.d(TAG, "La actividad muere.")
     }
+
+    private fun initEvent(){
+        boton.setOnClickListener{
+            Toast.makeText(this, "Hola!!!!",Toast.LENGTH_LONG).show()
+        }
+        boton3.setOnClickListener(this)
+
+    }
+    fun saludar(view: View) {
+        if (view. id == R.id. iv_boton) {//podría quitarlo, porque sólo lo referencia un sólo botón desde la interfaz
+            Toast.makeText( this, "No quiero saludar!!!",Toast.LENGTH_LONG).show()
+        }
+        else{
+            Toast.makeText( this, "quiero saludar!!!",Toast.LENGTH_LONG).show()
+
+        }
+    }
+
+    override fun onClick(p0: View?) {
+        if (p0?.id == R.id.button3){
+            Toast.makeText(this,"Saludo boton3", Toast.LENGTH_LONG).show()
+        }else{
+            Toast.makeText(this,"Saludo boton4", Toast.LENGTH_LONG).show()
+
+        }
+    }
+
+
+
 }
